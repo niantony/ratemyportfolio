b090785d9e7b150462416aeb6a05924e4ba8d7ab
niantony <ni96@purdue.edu>
2021-06-26 18:43:54 -0500

    Navbar

---
+++ components/Navbar.js
@@ -0,0 +1,28 @@
+import styles from '../styles/Navbar.module.css'
+import Link from 'next/link'
+
+function Navbar() {
+    return (
+        <div className={styles.navbar}>
+            <ul className={styles.menu}>
+                <li className={styles.item}>
+                    <Link href='/'>
+                        Home
+                    </Link>
+                </li>
+                <li className={styles.item}>
+                    <Link href='/explore'>
+                        Explore
+                    </Link>
+                </li>
+                <li className={styles.item}>
+                    <Link href='/about'>
+                        About Us
+                    </Link>
+                </li>
+            </ul>
+        </div>
+    )
+}
+
+export default Navbar
--- pages/index.js
+++ pages/index.js
@@ -13,7 +13,7 @@ export default function Home() {
 
       <main className={styles.main}>
         <h1 className={styles.title}>
-          Share and Explore <br /> Portolios
+          Share and Explore <br /> Investment Portolios
         </h1>
 
         <p className={styles.description}>
