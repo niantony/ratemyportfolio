d01451cbfbdfe5ee3ca844d63a5f7b22bf0564de
niantony <ni96@purdue.edu>
2021-06-26 12:42:11 -0500

    Init

---
+++ pages/index.js
@@ -0,0 +1,29 @@
+import Head from 'next/head'
+import Image from 'next/image'
+import styles from '../styles/Home.module.css'
+
+export default function Home() {
+  return (
+    <div className={styles.container}>
+      <Head>
+        <title>MarketShare</title>
+        <meta name="description" content="Stock Market Portfolio Sharing" />
+        <link rel="icon" href="/favicon.ico" />
+      </Head>
+
+      <main className={styles.main}>
+        <h1 className={styles.title}>
+          Share and Explore <br /> Portolios
+        </h1>
+
+        <p className={styles.description}>
+          Stock market portfolio sharing made easy
+        </p>
+
+        <div className={styles.button}>
+          Explore
+        </div>
+      </main>
+    </div>
+  )
+}
