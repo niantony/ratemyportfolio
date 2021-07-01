d86ac8ac87457d674744d803e676ed43a95dda16
niantony <ni96@purdue.edu>
2021-07-01 14:15:21 -0500

    User registration validation

--- pages/api/user/index.js
+++ pages/api/user/index.js
@@ -1,25 +1,35 @@
 import dbConnect from '../../../utils/dbConnect';
 import User from '../../../models/User';
+const bcrypt = require('bcryptjs')
+const { registerValidation } = require('../../validation')
 
 dbConnect();
-console.log('user api')
 
 export default async (req, res) => {
     const { method } = req;
 
     switch (method) {
-        case 'GET':
-            try {
-                const users = await User.find({});
-
-                res.status(200).json({ success: true, data: users })
-            } catch (error) {
-                res.status(400).json({ success: false });
-            }
-            break;
+        // User registration
         case 'POST':
+            // Validating request data
+            const {error} = registerValidation(req.body)
+                if (error) return res.status(400).json({ success: false, message: error.details[0].message})
+
+            // Checking if user is already in database
+            const emailExists = await User.findOne({ email: req.body.email })
+                if (emailExists) return res.status(400).send('Email already exists')
+
+            // Hashing password
+            const salt = await bcrypt.genSalt(10);
+            const hashPassword = await bcrypt.hash(req.body.password, salt)
+
+            // Creating user
             try {
-                const user = await User.create(req.body);
+                const user = await User.create({
+                    name: req.body.name,
+                    email: req.body.email,
+                    password: hashPassword
+                });
                 res.status(201).json({ success: true, data: user })
             } catch (error) {
                 res.status(400).json({ success: false });
