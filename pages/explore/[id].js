196bda08a8e503f251986f015abd4bc5ec0375a6
niantony <ni96@purdue.edu>
2021-07-21 14:31:53 -0500

    Pie Chart Fixes

--- pages/explore/[id].js
+++ pages/explore/[id].js
@@ -38,7 +38,6 @@ const Portfolio = (props) => {
 
       return (
         <div className={styles.container}>
-          {console.log(portfolio)}
           <div className={styles.info_container}>
             <h1 className={styles.title}>{portfolio.title.toUpperCase()}</h1>
             <p className={styles.subtitle}>{portfolio.stocks[0].name.toUpperCase()}, {portfolio.stocks[1].name.toUpperCase()}, {portfolio.stocks[2].name.toUpperCase()}...</p>
@@ -50,7 +49,7 @@ const Portfolio = (props) => {
             portfolio.stocks.map(stock => {
               const temp = {
                 title: stock.name,
-                value: parseInt(stock.percent),
+                value: stock.percent,
                 color: color[stockList.length]
               }
               stockList.push(temp)
