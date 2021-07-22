f752675702a2ce0332e5e2bcb9ebcbcbb03d0d8c
niantony <ni96@purdue.edu>
2021-07-22 15:33:06 -0500

    Portfolio Creation Page complete

--- pages/create.js
+++ pages/create.js
@@ -4,6 +4,7 @@ import 'firebase/firestore'
 import { useAuthState } from "react-firebase-hooks/auth";
 import { v4 as uuidv4 } from 'uuid';
 import styles from '../styles/Create.module.css'
+import Auth from '../components/Auth'
 
 export default function CreatePortfolio() {
   const [user, loading, error] = useAuthState(firebase.auth())
@@ -12,7 +13,8 @@ export default function CreatePortfolio() {
   const [notification, setNotification] = useState('');
   const [stocks, setStocks] = useState([])
   const [input, setInput] = useState('')
-  const [percent, setPercent] = useState(0)
+  const [percent, setPercent] = useState()
+  const [percentTotal, setPercentTotal] = useState(0)
 
   const saveInput = (e) => {
     setInput(e.target.value)
@@ -23,13 +25,14 @@ export default function CreatePortfolio() {
   }
 
   const addStock = (e) => {
-    if (percent < 100 && percent > 0) {
+    if (percent < 100 && percent > 0 && input !== "") {
       stocks.push({
         name: input,
         percent: percent
       })
+      setPercentTotal(percentTotal + percent)
       setInput("")
-      setPercent(0)
+      setPercent("")
     } 
     else if (percent < 1) {
       e.preventDefault()
@@ -38,6 +41,10 @@ export default function CreatePortfolio() {
     else if (percent > 100) {
       e.preventDefault()
       setNotification("Percentage needs to be less than 100")
+    }
+    else if (input === "") {
+      e.preventDefault()
+      setNotification("Missing stock ticker/name")
     }
   }
 
@@ -96,42 +103,79 @@ export default function CreatePortfolio() {
     }
   }
 
-  return (
-    <div>
-      <h2>Create Portfolio</h2>
-      {notification}
-      <div>
-          Add Stock<br />
-          <input type="text" onChange={saveInput} value={input} placeholder="Stock Ticker or Name..."/>
-          <br/>
-          <input type="number" onChange={savePercent} value={percent} placeholder="Percent..."/>
-          <br/>
-          <button onClick={addStock}>Add Stock</button>
+  if (user) {
+    return (
+      <div className={styles.create_container}>
+        <h1>Create a Portfolio</h1>
+          <div className={styles.container}>
+            <div className={styles.row}>
+              <form className={styles.form} onSubmit={handleSubmit}>
+                <h2>PORTFOLIO INFO</h2>
+                <div>
+                  <h4>TITLE</h4>
+                  <input className={styles.input} type="text" value={title} placeholder="Title..."
+                  onChange={({target}) => setTitle(target.value)} />
+                </div>
+                <div>
+                  <h4>DESCRIPTION</h4>
+                  <textarea className={styles.input_desc} value={description} placeholder="Description..."
+                  onChange={({target}) => setDescription(target.value)} />
+                </div> 
+                <button className={styles.button} type="submit">CREATE PORTFOLIO</button>
+              </form>
+  
+              <div className={styles.add_container}>
+                <h2>ADDING STOCKS</h2>
+                <h4>STOCK TICKER/NAME</h4>
+                <input className={styles.input} type="text" onChange={saveInput} value={input} placeholder="Stock Ticker or Name..."/>
+                <h4>PERCENTAGE</h4>
+                <input className={styles.input} type="number" onChange={savePercent} value={percent} placeholder="Percent..."/>
+                <br />
+                <button className={styles.button} onClick={addStock}>+</button>
+                <p className={styles.requirements}>Portfolio must have at least 5 stocks and the sum of the percentages must equal 100.</p>
+              </div>
+            </div>
+  
+            {stocks.length > 0 ?  
+              <div className={styles.table_container}>
+                <div className={styles.table}>
+                  <ul className={styles.heading}>
+                    <li className={styles.item_one}>
+                      TICKER/NAME
+                    </li>
+                    <li className={styles.item_two}>
+                      PERCENT ({percentTotal}%)
+                    </li>
+                  </ul>
+                </div>
+  
+                {stocks.map(stock => {
+                      return (
+                        <div>
+                          <ul className={styles.menu}>
+                            <li className={styles.item_name} key={stock.name}>
+                              {stock.name} 
+                            </li>
+                            <li className={styles.item_percent} key={stock.percent}>
+                              {stock.percent}%
+                            </li>
+                            <button className={styles.delete_button} onClick={() => {
+                              setStocks(stocks.filter(data => data.name !== stock.name));
+                            }}>x</button>
+                          </ul>
+                        </div>
+                      )  
+                    }
+                  )}
+              </div>
+            : <></>}
+          </div>
+          <p className={styles.notification}>{notification}</p>
       </div>
-      <form onSubmit={handleSubmit}>
-        <div>
-          Title<br />
-          <input type="text" value={title} 
-           onChange={({target}) => setTitle(target.value)} />
-        </div>
-        <div>
-          Description<br />
-          <textarea value={description} 
-           onChange={({target}) => setDescription(target.value)} />
-        </div>
-        <button type="submit">Save</button>
-      </form>
-      <ul>
-        {stocks.map(stock => {
-            return (
-              <li key={stock.name}>
-                {stock.name} 
-                {stock.percent}
-              </li>
-            )  
-          }
-        )}
-      </ul>
+    )
+  } else return (
+    <div className={styles.auth_container}>
+      <Auth />
     </div>
   )
 }
--- pages/index.js
+++ pages/index.js
@@ -1,5 +1,4 @@
 import Head from 'next/head'
-import Image from 'next/image'
 import Link from 'next/link'
 import styles from '../styles/Home.module.css'
 
@@ -7,12 +6,6 @@ export default function Home() {
   return (
     <>
       <div className={styles.container}>
-        <Head>
-          <title>MarketShare</title>
-          <meta name="description" content="Stock Market Portfolio Sharing" />
-          <link rel="icon" href="/favicon.ico" />
-        </Head>
-
         <main className={styles.main}>
           <h1 className={styles.title}>
             Share and Rate <br /> Investment Portolios
