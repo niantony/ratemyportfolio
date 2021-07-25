175a223ba6d84b700b03ede0fea63705bb23c73d
niantony <ni96@purdue.edu>
2021-07-25 13:49:52 -0500

    Homepage css

--- pages/index.js
+++ pages/index.js
@@ -5,10 +5,10 @@ import styles from '../styles/Home.module.css'
 export default function Home() {
   return (
     <div className={styles.home_container}>
-      <div className={styles.container}>
+      <div className={styles.main_container}>
         <main className={styles.main}>
           <h1 className={styles.title}>
-            Share and Rate <br /> Investment Portolios
+            Share and Rate <br /> Investment <span>Portolios</span>
           </h1>
 
           <p className={styles.description}>
@@ -22,6 +22,32 @@ export default function Home() {
           </div>
         </main>
       </div>
+
+      <div className={styles.container}>
+        <div className={styles.row}>
+          <div className={styles.col}>
+            <h1>Explore and Rate Portfolios</h1>
+            <h2>RateMyPortfolio offers the best way to explore and rate countless user created portfolios. 
+              Get started by signing up! </h2>
+            <div className={styles.button_container}>
+              <div className={styles.signup}>
+                <Link href='/auth'>
+                  Sign Up
+                </Link>
+              </div>
+              <div className={styles.login}>
+                <Link href='/auth'>
+                  Login
+                </Link>
+              </div>
+            </div>
+          </div>
+          <div className={styles.col}>
+            <img className={styles.image} src="/explore.png"/>
+          </div>
+        </div>
+      </div>
+
       <div className={styles.card_container}>
         <div className={styles.card}>
           <h2>Dividend Growth 🌱</h2>
